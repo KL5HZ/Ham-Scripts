d31666ffe88206c95dcb4522fabbee98b482335e
KL5HZ <anthony.s.woodward1@gmail.com>
2024-11-27 21:38:06 -0600

    Update wsjtx.sh

--- wsjtx.sh
+++ wsjtx.sh
@@ -2,6 +2,6 @@
 wget https://sourceforge.net/projects/wsjt/files/wsjtx-2.6.1/wsjtx_2.6.1_amd64.deb/download
 
 #Install WSJTX
-sudo dpkg -i wsjtx_2.6.1_amd64.deb
+sudo dpkg -i wsjtx_2.6.1_amd64.deb/download
 
 #If error when trying to install WSJTX, run the remove command
