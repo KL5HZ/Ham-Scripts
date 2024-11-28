abd5ef9ba1a92c59d37b76f1ab41677a58c4bcd7
KL5HZ <anthony.s.woodward1@gmail.com>
2024-11-27 21:16:25 -0600

    Create wsjtx.sh

---
+++ wsjtx.sh
@@ -0,0 +1,7 @@
+#Pull WSJTX from sourceforge site
+wget https://sourceforge.net/projects/wsjt/files/wsjtz-2.6.1/wsjtx_2.6.1_amd64.deb
+
+#Install WSJTX
+-sudo dpkg -i wsjtx_2.6.1_amd64.deb
+
+#If error when trying to install WSJTX, run the remove command
