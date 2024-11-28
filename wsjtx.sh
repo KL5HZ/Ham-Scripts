43a8a42aedc74b1e5fa5dc14cc4b3f8e865dd107
KL5HZ <anthony.s.woodward1@gmail.com>
2024-11-27 21:23:12 -0600

    Update wsjtx.sh site url

--- wsjtx.sh
+++ wsjtx.sh
@@ -1,5 +1,5 @@
 #Pull WSJTX from sourceforge site
-wget https://sourceforge.net/projects/wsjt/files/wsjtz-2.6.1/wsjtx_2.6.1_amd64.deb
+wget https://sourceforge.net/projects/wsjt/files/wsjtx-2.6.1/wsjtx_2.6.1_amd64.deb/download
 
 #Install WSJTX
 -sudo dpkg -i wsjtx_2.6.1_amd64.deb
