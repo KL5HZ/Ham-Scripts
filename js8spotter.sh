fb7b52f2d790852f5388d78adeabc5ca70701d50
KL5HZ <anthony.s.woodward1@gmail.com>
2024-11-28 01:39:29 -0600

    Update js8spotter.sh separate line for python package installs and updated program version install

--- js8spotter.sh
+++ js8spotter.sh
@@ -7,28 +7,32 @@ sudo apt-get update
 
 #Install required python packages
 echo "Installing python packes..."
-sudo apt install python3-tk python3-pil python-pil.imagetk python3-requests python3-tksnack
+sudo apt install python3-tk
+sudo apt install python3-pil
+sudo apt install python-pil.imagetk
+sudo apt install python3-requests
+sudo apt install python3-tksnack
 
 # Download js8spotter package
 echo "Downloading js8spotter zip..."
-wget "https://kf7mix.com/file/js8spotter/js8spotter-111b.zip"
+wget "https://kf7mix.com/files/js8spotter/js8spotter-112b.zip"
 
 #Unzip js8spotter
 echo "Unzipping js8spotter..."
-unzip js8spotter-111b.zip
+unzip js8spotter-112b.zip
+
+#Move into js8spotter directory
+echo "Changing directory..."
+cd js8spotter-112b/
+
+#Make js8spotter executable
+echo "Making js8spotter executable..."
+chmod +x js8spotter.py
 
 # Clean up unnecessary files
 echo "Cleaning up..."
 sudo apt-get autoremove -y
 sudo apt-get clean
-
-#Move into js8spotter directory
-echo "Changing directory..."
-cd js8spotter-111b/
-
-#Make js8spotter executable
-echo "Making js8spotter executable..."
-chmod +x js8spotter.py
 
 # Verify installation
 echo "Verifying js8spotter installation..."
@@ -36,5 +40,5 @@ js8spotter --version
 
 echo "Installation complete. Enjoy using js8spotter!"
 echo "JS8Spotter is an incomplete product, resulting in not being able to make shortcut for program."
-echo "To start JS8Spotter, right click and choose to run as a program inside js8spotter-111b directory."
+echo "To start JS8Spotter, right click and choose to run as a program inside js8spotter-112b directory."
 EOF
