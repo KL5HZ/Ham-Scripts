28301c8dbab519d232b3a9c391285b0b41c5f2b6
Anthony <anthony.s.woodward1@gmail.com>
2024-11-30 22:41:20 -0600

    Update js8spotter.sh
    Removed comments at end about broken program.
    
    Simplified commands by including full file path, resulting in not needing to move into directory.
    
    Grammar fixes.

--- js8spotter.sh
+++ js8spotter.sh
@@ -21,17 +21,12 @@ wget "https://kf7mix.com/files/js8spotter/js8spotter-112b.zip"
 echo "Unzipping js8spotter..."
 unzip js8spotter-112b.zip
 
-#Move into js8spotter directory
-echo "Changing directory..."
-cd js8spotter-112b/
-
 #Make js8spotter executable
 echo "Making js8spotter executable..."
-chmod +x js8spotter.py
+chmod a+x ~/js8spotter-112b/js8spotter.py
 
 #Create desktop shortcut
 echo "Creating js8spotter desktop shortcut..."
-cd ~/Desktop
 echo "[Desktop Entry]
 Version=1.0
 Name=JS8Spotter
@@ -40,13 +35,14 @@ Exec=python3 ~/js8spotter-112b/js8spotter.py
 Icon=~/js8spotter-112b/js8spotter.ico
 Path=~/js8spotter-112b/
 Terminal=false
-Type=Application" > JS8Spotter.desktop
+Type=Application" >> ~/Desktop/JS8Spotter.desktop
 
 #Make file exectuable
 chmod a+x ~/js8spotter-112b/js8spotter.py
 gio set ~/Desktop/JS8Spotter.desktop  metadata::trusted true
-cd ~/Desktop
-chmod +x ~/Desktop/JS8Spotter.desktop
+
+#Make Desktop shortcut executable
+chmod a+x ~/Desktop/JS8Spotter.desktop
 
 # Clean up unnecessary files
 echo "Cleaning up..."
@@ -58,8 +54,4 @@ echo "Verifying js8spotter installation..."
 js8spotter --version
 
 echo "Installation complete. Enjoy using js8spotter!"
-echo "JS8Spotter is an incomplete product, resulting in not being able to make shortcut for program."
-echo "To start JS8Spotter, there are two options:
-1: Right click and choose to run as a program inside js8spotter-112b directory
-2: Run the program in terminal with 'python3 /path/to/js8spotter.py."
 EOF
