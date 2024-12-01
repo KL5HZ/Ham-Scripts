1e6354bc73fad2dbb2a637a1bb86cc6d0c61562a
Anthony <anthony.s.woodward1@gmail.com>
2024-11-30 23:15:05 -0600

    Update js8spotter.sh
    Moved into directory for shortcut execute command

--- js8spotter.sh
+++ js8spotter.sh
@@ -31,14 +31,15 @@ Exec=python3 ~/js8spotter-112b/js8spotter.py
 Icon=~/js8spotter-112b/js8spotter.ico
 Path=~/js8spotter-112b/
 Terminal=false
-Type=Application" >> ~/Desktop/JS8Spotter.desktop
+Type=Application" > ~/Desktop/JS8Spotter.desktop
 
 #Make file exectuable
 chmod a+x ~/js8spotter-112b/js8spotter.py
 gio set ~/Desktop/JS8Spotter.desktop  metadata::trusted true
 
 #Make Desktop shortcut executable
-sudo chmod a+x ~/Desktop/JS8Spotter.desktop
+cd Desktop/
+sudo chmod a+x JS8Spotter.desktop
 
 # Clean up unnecessary files
 echo "Cleaning up..."
